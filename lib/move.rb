b44e9cbcc09f1b4945358e00edb4c07ba7ae090a
Richard Hanolt <rhanolt@gmail.com>
2020-04-08 01:10:34 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,16 +1,12 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # code your input_to_index and move method here!
-def input_to_index(user_input = gets.strip)
-  index = user_input.to_i - 1
-end
+def input_to_index
 
-def move(board, index, char = "X")
-  board[index] = char
 end
